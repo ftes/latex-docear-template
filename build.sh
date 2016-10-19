8a06263f955bf6fb1805e7d6aa22b2922c720fbe
Fredrik Teschke <f@ftes.de>
2016-10-19 09:49:11 +0200

    build pdf only if mindmap succesfully transformed into tex

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-xsltproc -o body.tex mm2latex/mm2latex.xsl body.mm
-arara thesis.tex
+xsltproc -o body.tex mm2latex/mm2latex.xsl body.mm && arara thesis.tex
 #okular thesis.pdf &> /dev/null
